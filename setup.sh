8e616b3073098d2ee3a930986a1c28767d316736
Man Wei Tam <manweitam@gmail.com>
2023-05-02 22:18:32 -0500

    Configure environment.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+
+## Install pytorch with cuda (https://pytorch.org/get-started/locally/)
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
